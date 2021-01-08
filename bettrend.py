440dfefa9df722e7b21c305f0af1fb970e76e9bf
lmnres <luc.mouneyres@gmail.com>
2021-01-07 22:42:10 -0600

    Premiere Version Fonctionelle

--- bettrend.py
+++ bettrend.py
@@ -25,6 +25,7 @@ def initialize(list):
         list.append(team)
     return list
 
+
 # Retrieve html from a given url and converts to lxml
 def make_soup(url):
     try:
@@ -33,26 +34,32 @@ def make_soup(url):
         return None
     return BeautifulSoup(html, "lxml")
 
-#
-def update_score(Team,soup):
+
+# Update odd from Betclic and tuple {odd:time} in team.data
+def update_odd(Team,soup):
     for i in range(40):
         link_test_name = "ng-tns-c0-{}".format(i)
-        team_name = soup.find('span', {"class": link_test_name}).text
-        time = datetime.now()
-        if team_name[:4] in Team.name:
-            link_test_odd = "oddValue ng-tns-c0-{} ng-star-inserted".format(i)
-            odd_value = soup.find('span', {"class": link_test_odd}).text
-            odd_int_value = int(odd_value)
-            Team.data['odd'] = odd_int_value
-            Team.data['time'] = time
-        else:
-            Team.data['odd'] = 0
-            Team.data['time'] = time
+        if soup.find('span', {"class": link_test_name}).text:
+            span = soup.find('span', {"class": link_test_name}).text
+            team_name = span
+            time = datetime.now()
+            if team_name[1:4] == Team.name[:3]:
+                link_test_odd = "oddValue ng-tns-c0-{} ng-star-inserted".format(i)
+                odd_value = soup.find('span', {"class": link_test_odd}).text
+                odd_point_value = odd_value.replace(",",".")
+                odd_float_value = float(odd_point_value)
+                Team.data['odd'] = odd_float_value
+                Team.data['time'] = time
+            else:
+                Team.data['odd'] =0
+                Team.data['time'] = time
     return
+
 
 #Code already used to initialize list of teams
 initialize(list_nba_teams_data)
 soup = make_soup(betclic_nba_url)
+print(soup)
 for team in list_nba_teams_data:
-    update_score(team,soup)
+    update_odd(team,soup)
     print (team.data)
