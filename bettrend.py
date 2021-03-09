9948dbadd16050525561b7e6e58d38aa184fd7a8
Luc Mouneyres <lucmouneyres@Lucs-MacBook-Pro.local>
2021-03-09 17:42:39 +0100

    refresh new macbook

--- bettrend.py
+++ bettrend.py
@@ -9,10 +9,10 @@ nba_teams = ['Atlanta Hawks','Boston Celtics','Brooklyn Nets','Charlotte Hornets
 list_nba_teams_data=[]
 # Creation of Team Class
 class Team:
-def __init__(self, name,odd,time):
-self.name = name
-self.odd = odd
-self.time = time
+    def __init__(self, name,odd, time):
+        self.name = name
+        self.odd = odd
+        self.time = time
 
 # To be used only once to initialize teams and data
 # def initialize(list):
@@ -24,46 +24,47 @@ self.time = time
 
 # Retrieve html from a given url and converts to lxml
 def make_soup(url):
-try:
-html = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"}).content
-except:
-return None
-return BeautifulSoup(html, "lxml")
+    try:
+        html = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.80 Safari/537.36"}).content
+    except:
+        return None
+    return BeautifulSoup(html, "lxml")
 
 # Update odd from Betclic and tuple {odd:time} in team.data
 def update_odd(Team,soup):
-for i in range(40):
-link_test_name = "ng-tns-c0-{}".format(i)
-time = datetime.now()
-# if soup.find('span', {"class": link_test_name}).text[1:4] in Team.name[:3]:
-if soup.find('span', {"class": link_test_name}).text[1:4] in Team.name[:3]:
-link_test_odd = "oddValue ng-tns-c0-{} ng-star-inserted".format(i)
-odd_value = soup.find('span', {"class": link_test_odd}).text
-if odd_value != "-":
-odd_point_value = odd_value.replace(",",".")
-odd_float_value = float(odd_point_value)
-# name = Team.name
-Team.odd = odd_float_value
-Team.time = time
-return Team
+    for i in range(40):
+        link_test_name = "ng-tns-c0-{}".format(i)
+        time = datetime.now()
+        # if soup.find('span', {"class": link_test_name}).text[1:4] in Team.name[:3]:
+        if soup.find('span', {"class": link_test_name}).text[1:4] in Team.name[:3]:
+            link_test_odd = "oddValue ng-tns-c0-{} ng-star-inserted".format(i)
+            odd_value = soup.find('span', {"class": link_test_odd}).text
+        if odd_value != "-":
+            odd_point_value = odd_value.replace(",",".")
+            odd_float_value = float(odd_point_value)
+        # name = Team.name
+        Team.odd = odd_float_value
+        Team.time = time
+    return Team
 
 #Code already used to initialize list of teams
 # initialize(list_nba_teams_data)
 time = datetime.now()
 for x in nba_teams:
-list_nba_teams_data.append(Team(name=x,odd=0,time=time))
-soup = make_soup(betclic_nba_url)
-print(soup)
-list_names=[]
-list_odds=[]
-list_times=[]
+    list_nba_teams_data.append(Team(name=x,odd=0,time=time))
+    soup = make_soup(betclic_nba_url)
+    print(soup)
+    list_names=[]
+    list_odds=[]
+    list_times=[]
+
 for team in list_nba_teams_data:
-list_names.append(team.name)
-list_odds.append(team.odd)
-list_times.append(team.time)
-update_odd(team,soup)
-list_names.append(team.name)
-list_odds.append(team.odd)
-list_times.append(team.time)
-data_teams=pd.DataFrame({'Team_name':list_names,'odd':list_odds,'time':list_times})
-print(data_teams)
+    list_names.append(team.name)
+    list_odds.append(team.odd)
+    list_times.append(team.time)
+    update_odd(team,soup)
+    list_names.append(team.name)
+    list_odds.append(team.odd)
+    list_times.append(team.time)
+    data_teams=pd.DataFrame({'Team_name':list_names,'odd':list_odds,'time':list_times})
+    print(data_teams)
